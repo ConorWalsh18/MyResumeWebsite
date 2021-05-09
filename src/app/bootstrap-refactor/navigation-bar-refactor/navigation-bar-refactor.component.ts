3dfc3a939fd904e213f7736107dd67ec432ae432
Walsh,Conor <cwalsh@vertafore.com>
2021-05-08 21:24:07 -0600

    Added functionality to navbar

--- src/app/bootstrap-refactor/navigation-bar-refactor/navigation-bar-refactor.component.ts
+++ src/app/bootstrap-refactor/navigation-bar-refactor/navigation-bar-refactor.component.ts
@@ -10,6 +10,23 @@ export class NavigationBarRefactorComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    var navBar = document.getElementById("navbarNav");
+    var navBarButton = document.getElementById("navBarButton");    
+
+    document.addEventListener('click', () => {      
+      if (navBar.classList.contains("show")) {        
+        navBarButton.click();
+      }
+    });  
   }
 
+  scrollToSecton(section, element, offset = 0) {     
+    // element.target.classList.add("active");  
+    // document.getElementById(section).scrollIntoView({behavior: "smooth", block: "start", inline: "nearest"});        
+    
+    $('html, body').animate({
+      // scrollTop: $(section).offset().top - 70
+      scrollTop: $(section).offset().top - offset
+    }, 0);
+  }
 }
