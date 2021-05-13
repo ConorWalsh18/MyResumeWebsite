e8cf56d049d6f7d13a8e6d96d5c5b36515acd671
Walsh,Conor <cwalsh@vertafore.com>
2021-05-12 18:45:02 -0600

    Updated personal projects page

--- src/app/personal-projects/personal-projects.component.ts
+++ src/app/personal-projects/personal-projects.component.ts
@@ -3,13 +3,19 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'personal-projects',
   templateUrl: './personal-projects.component.html',
-  styleUrls: ['./personal-projects.component.css']
+  styleUrls: ['./personal-projects.component.scss']
 })
 export class PersonalProjectsComponent implements OnInit {
 
   constructor() { }
 
   ngOnInit(): void {
+    var cards = document.querySelectorAll(".project-card");
+    cards.forEach(function(card) {
+      card.addEventListener("click", function() {
+        console.log(card)
+        card.classList.toggle("is-flipped");
+      });
+    });
   }
-
 }
