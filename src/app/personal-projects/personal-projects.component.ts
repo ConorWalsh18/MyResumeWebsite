46ecb62874bc8f022924c91ca8c45b2209bc3bb0
Walsh,Conor <cwalsh@vertafore.com>
2021-05-15 17:31:44 -0600

    Changed about me picture colors

--- src/app/personal-projects/personal-projects.component.ts
+++ src/app/personal-projects/personal-projects.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FadeAndMoveInService } from '../fade-and-move-in.service';
 
 @Component({
   selector: 'personal-projects',
@@ -7,9 +8,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PersonalProjectsComponent implements OnInit {
 
-  constructor() { }
+  constructor(private fadeAndMoveIn: FadeAndMoveInService) { }
 
   ngOnInit(): void {
+    var projectSection = document.getElementById("projectSection");
+    var elements = projectSection.getElementsByClassName("fade-and-move-in");
+
+    window.addEventListener('scroll', () => {      
+      //TODO: Fix when the "show" class is removed
+      // if (window.pageYOffset > projectSection.offsetTop + window.innerHeight - 20 || window.pageYOffset < projectSection.offsetTop / 2) {
+      //     for (var i = 0; i < elements.length; i++) {
+      //       // elements[i].classList.remove("show")
+      //     }
+      // }
+      // else {
+      //   this.fadeAndMoveIn.start(elements);
+      // }
+
+      this.fadeAndMoveIn.start(elements);
+    });
+
     var cards = document.querySelectorAll(".project-card");
     cards.forEach(function(card) {
       card.addEventListener("click", function() {        
