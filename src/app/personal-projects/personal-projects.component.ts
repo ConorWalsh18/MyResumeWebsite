63e777333ff197afb350c0c5d2f33984f7fdee83
Walsh,Conor <cwalsh@vertafore.com>
2021-06-24 17:25:52 -0600

    Updated projects page

--- src/app/personal-projects/personal-projects.component.ts
+++ src/app/personal-projects/personal-projects.component.ts
@@ -6,20 +6,35 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./personal-projects.component.scss']
 })
 export class PersonalProjectsComponent implements OnInit {
-  
+    
+  playVideo: boolean = false;
+
   constructor() { }
 
   ngOnInit(): void {    
-    var cards = document.querySelectorAll(".project-card");
+    var cards = document.querySelectorAll(".project-card");    
 
     cards.forEach(function(card) {
       card.addEventListener("click", function(event) {        
         var clickedElement = event.target as HTMLElement;
 
-        if (clickedElement.nodeName != "VIDEO" && clickedElement.nodeName != "A") {          
+        console.log(clickedElement);
+        console.log(clickedElement.id);
+        console.log(clickedElement.nodeName);
+
+        if (clickedElement.nodeName != "VIDEO" && clickedElement.nodeName != "A" && clickedElement.id != "play-video") {          
           card.classList.toggle("is-flipped");
+        }
+        else if (clickedElement.nodeName == "IMG" && clickedElement.id == "play-video") {
+          var demoVideo = document.getElementById($(clickedElement).attr("name"));
+          console.log(demoVideo);
+
+          clickedElement.classList.add("hide");
+          demoVideo.classList.remove("hide");
+          demoVideo.getElementsByTagName("video")[0].play();
         }
       });
     });
   }
 }
+ 
